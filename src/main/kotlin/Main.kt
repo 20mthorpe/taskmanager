df7d9fa1bd201246b486bad63f667f3409ed86ef
20mthorpe <tho20019@byui.edu>
2023-06-03 17:00:42 -0600

    Add Comments, Clean up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,23 @@
 fun main(args: Array<String>) {
+
+    // Welcome
     println("Welcome to task manager, where we can help you keep track of your to do list. ")
+
+    // For menu loop
     var run = true
+
+    // Create list to hold tasks
     var tasks = ArrayList<Task>()
+
+    // Create task types
     val schoolTask = Type("School", 1)
     val homeTask = Type("Home", 2)
     val miscTask = Type("Miscellaneous", 3)
 
+    // Run the main program
     while(run)
     {
+        //Display Menu
         println("\nPlease choose an option from the following menu:")
         println("1. Create a new task")
         println("2. Display all tasks")
@@ -16,37 +26,46 @@ fun main(args: Array<String>) {
         println("5. Exit")
         val input = readLine()
 
+        // Create a new task
         if (input == "1") {
 
+            // Get description
             println("What is a short description of your task? ")
             val desc = readLine()
-            var noTask = true
+
+            //Get task type
+            var noType = true
             var type = Type("", 0)
-            while (noTask) {
+            while (noType) {
 
                 println("Which of the following categories does this task fit into? ")
                 println("1. School")
                 println("2. Home")
                 println("3. Miscellaneous")
+
                 val inputType = readLine()
                 if (inputType == "1") {
                     type = schoolTask
-                    noTask = false
+                    noType = false
                 } else if (inputType == "2") {
                     type = homeTask
-                    noTask = false
+                    noType = false
                 } else if (inputType == "3") {
                     type = miscTask
-                    noTask = false
+                    noType = false
                 } else {
                     println("Invalid input. Please select an option from the list. ")
                 }
             }
+            // Create task, add to list, sort list by priority
             var task = Task(desc, type)
             tasks.add(task)
-        }
-        else if (input == "2"){
             tasks.sortBy{ it.type.getPriority() }
+        }
+
+        // Display all tasks
+        else if (input == "2"){
+
             var count = 1
             var schoolPrint = false
             var homePrint = false
@@ -69,12 +88,17 @@ fun main(args: Array<String>) {
                 count += 1
             }
         }
+
+        // Display only incomplete tasks
         else if(input == "3"){
-            tasks.sortBy{ it.type.getPriority() }
+
             var count = 1
+
+            // We only want to print the type once so use boolean variables to do this
             var schoolPrint = false
             var homePrint = false
             var miscPrint = false
+
             for(task in tasks){
                 if(!task.getIsComplete()) {
                     if (task.type.getPriority() == 1 && !schoolPrint) {
@@ -93,9 +117,13 @@ fun main(args: Array<String>) {
                 }
             }
         }
+
+        // Complete a task
         else if (input == "4"){
             println("Which task would you like to complete? (Program will not display tasks that are already complete)")
             var count = 1
+
+            // Display tasks
             for(task in tasks)
             {
                 if(!task.getIsComplete()){
@@ -104,14 +132,20 @@ fun main(args: Array<String>) {
                 }
                 count += 1
             }
+            //Get task to be completed, then complete the task
             val indexInput = readln()
             var index = indexInput.toInt()
-            index -= 1
+            index -= 1//change to an index
             tasks[index].completeTask()
         }
+
+        // Exit the loop and program
         else if (input == "5"){
+            // end while loop
             run = false
         }
+
+        // Invalid input
         else{
             println("Invalid input. Please select an option from the list. ")
         }
